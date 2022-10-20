e2bdb113361bb5b0e5faeac5209ba7cef88b2d35
torbenwb <torbenwbernhard@gmail.com>
2022-10-20 10:56:52 -0700

    new build

--- Build/TowerDefenseBuild.framework.js
+++ Build/TowerDefenseBuild.framework.js
@@ -1293,7 +1293,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2429136;
+STATICTOP = STATIC_BASE + 2429536;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3347,7 +3347,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2429136;
+var STATIC_BUMP = 2429536;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
